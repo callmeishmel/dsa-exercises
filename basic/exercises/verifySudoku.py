31cbe12c7410f509ddd900d828afb070c526ae37
Ismael Guajardo <ish.gdo@gmail.com>
2024-02-18 16:23:45 -0700

    added exercise notes

--- basic/exercises/verifySudoku.py
+++ basic/exercises/verifySudoku.py
@@ -1,3 +1,23 @@
+# Clarifications
+# We don't need to solve the puzzle
+# We only need to verify that the current state of the puzzle is valid
+# What makes an invalid board
+#   - Repeat numbers
+#   - Are non numbers something we need to account for?
+#   - Are all arrays guaranteed to have the same number (9x9)
+
+# Naive method
+# Iterate through each row and check if each num is unique in that space
+# Iterate through each column and check if each num is unique in that space
+# We don't need to check non number characters
+#   - We can reduce the column/row array to just nums and check if unique (Unnecessary(?))
+#   - We can IF number THEN check ignoring other characters (Direct)
+# To check rows iterate through each array as they are
+# To check columns iterate through each array at a specific index
+
+# TODO This fails because I forgot to account for the rule where values
+#      can't repeat inside a 3x3 grid 
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = self.areRowsValid(board)
